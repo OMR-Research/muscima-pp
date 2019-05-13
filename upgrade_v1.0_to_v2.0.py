9368fa11dbcabd746e212d0762df23c78493be81
Alexander Pacha <alexander.pacha@gmail.com>
2019-05-13 15:35:46 +0200

    Added split of notehead-empty into noteheadHalf and noteheadWhole, depending on whether it has a stem attached or not. See #2

--- upgrade_v1.0_to_v2.0.py
+++ upgrade_v1.0_to_v2.0.py
@@ -4,10 +4,13 @@ from copy import deepcopy
 from xml.etree.ElementTree import parse, ElementTree, Element
 import xml.dom.minidom
 
+from muscima.cropobject import CropObject
+from muscima.io import parse_cropobject_list
 from tqdm import tqdm
+from typing import List, Dict
 
 
-def upgrade_xml_file(element_tree: ElementTree) -> ElementTree:
+def upgrade_xml_file(element_tree: ElementTree, crop_objects: List[CropObject]) -> ElementTree:
     nodes = Element("Nodes", attrib={'xmlns:xsi': "http://www.w3.org/2001/XMLSchema-instance",
                                      "xmlns:xsd": "http://www.w3.org/2001/XMLSchema"})
 
@@ -30,13 +33,15 @@ def upgrade_xml_file(element_tree: ElementTree) -> ElementTree:
                      "ledger_line": "legerLine",
                      }
 
-    for crop_object in element_tree.findall("*/CropObject"):
+    id_to_cropobject_mapping = {o.objid: o for o in crop_objects}  # type: Dict[int, CropObject]
+
+    for crop_object_node in element_tree.findall("*/CropObject"):
         # Copy all values from an existing crop-object
-        node = deepcopy(crop_object)  # type: Element
+        node = deepcopy(crop_object_node)  # type: Element
 
         # Rename CropObject -> Node
         node.tag = "Node"
-
+        id = None
         for child in node:
             # Remove the leading ML prefix from MLClassName, in case it still exists
             if child.tag == "MLClassName":
@@ -47,6 +52,21 @@ def upgrade_xml_file(element_tree: ElementTree) -> ElementTree:
                 if child.text in class_mapping:
                     child.text = class_mapping[child.text]
 
+            if child.tag == "Id":
+                id = int(child.text)
+
+        # Split notehead-empty into noteheadHalf or noteheadWhole
+        crop_object = id_to_cropobject_mapping[id]
+        if crop_object.clsname == "notehead-empty":
+            notehead_has_a_stem_attached = False
+            for outgoing_object in crop_object.get_outlink_objects(crop_objects):  # type: CropObject
+                if outgoing_object.clsname == "stem":
+                    notehead_has_a_stem_attached = True
+
+            if notehead_has_a_stem_attached:
+                node.find("ClassName").text = "noteheadHalf"
+            else:
+                node.find("ClassName").text = "noteheadWhole"
 
         nodes.append(node)
 
@@ -82,8 +102,10 @@ if __name__ == "__main__":
         destination = os.path.join(destination_directory, destination_subdirectory)
 
         for annotation_file in tqdm(os.listdir(source), "Converting annotations"):
-            tree = parse(os.path.join(source, annotation_file))
-            upgraded_tree = upgrade_xml_file(tree)
+            annotation_file_path = os.path.join(source, annotation_file)
+            crop_objects = parse_cropobject_list(annotation_file_path)
+            tree = parse(annotation_file_path)
+            upgraded_tree = upgrade_xml_file(tree, crop_objects)
 
             upgraded_tree.write(os.path.join(destination, annotation_file), encoding="utf-8", xml_declaration=True)
             prettify_xml_file(os.path.join(destination, annotation_file))
